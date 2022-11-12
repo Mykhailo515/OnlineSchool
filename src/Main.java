519dc01557e96e8966a19f51e8d5cee397f804de
Mykhailo515 <mikhail.chursinov@gmail.com>
2022-11-12 10:27:22 +0200

    repo program

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,6 @@ import models.Lectures;
 
 public class Main {
     public static void main(String[] args) {
-//        Lectures lectures = new Lectures();
-//        Lectures secondLectures = new Lectures();
-//        Lectures thirdLectures = new Lectures();
-//
-//        System.out.println(Lectures.counter);
-//        Lectures fourthLectures = new Lectures();
-//        Lectures fifthLectures = new Lectures();
-
 
         Course course = new Course(2);
         Lectures lectures = new Lectures(course.getID());
