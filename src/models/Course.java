a68a6481918df5463a61c809d918197af1724b11
Mykhailo515 <mikhail.chursinov@gmail.com>
2023-01-31 10:39:30 +0200

    commit repo

--- src/models/Course.java
+++ src/models/Course.java
@@ -2,9 +2,11 @@ package models;
 
 public class Course extends SuperClass {
     private static int counterCourse;
+    private String name;
 
-    public Course(int id) {
+    public Course(int id, String name) {
         super(id);
+        this.name = name;
         counterCourse++;
     }
 }
